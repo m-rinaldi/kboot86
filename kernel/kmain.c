dbb20d784668eb7d3f85620ddbfef65aaffbf353
Jorge Rinaldi <jrg.rinaldi@gmail.com>
2016-03-06 18:17:52 +0100

    stub kernel: tracing marks removed and original screen kept

--- kernel/kmain.c
+++ kernel/kmain.c
@@ -1,19 +1,33 @@
 #include <stdint.h>
 #include <stddef.h>
 
-static volatile uint16_t *vga_mem;
+#define VGA_MEM_ADDR    0xb8000
+#define COLS            80
+#define ROWS            25
+#define ATTR            0x1f    // white on blue
 
-#define ATTR    0x1f    // white on blue
-void kmain(void)
+
+static inline unsigned int _xy2idx(unsigned int x, unsigned int y)
 {
-    const char *str = "sample kernel";
-    size_t i;
+    return y + COLS*x;
+}
 
-    vga_mem = (uint16_t *) 0xb8000;
-
-    for (i = 0; str[i]; i++)
-        vga_mem[i] = (uint16_t) ATTR << 8 | str[i];
+void bsod(void)
+{
+    volatile uint16_t *vga_mem;
+    int i, j;
     
+    vga_mem = (uint16_t *) VGA_MEM_ADDR;
+    for (i = 0; i < ROWS; i++)
+        for (j = 0; j < COLS; j++)
+            vga_mem[_xy2idx(i, j)] =
+                (uint16_t) ATTR << 8 | (0x00ff & vga_mem[_xy2idx(i, j)]);
+
     while (1)
         ;
+}
+
+void kmain(void)
+{
+    bsod();
 }
