266fc346ebd51e91c98523f80825e5ddeaa85b66
Jorge Rinaldi <jrg.rinaldi@gmail.com>
2016-04-17 14:43:24 +0200

    strcmp() implemented

--- include/string.h
+++ include/string.h
@@ -6,6 +6,7 @@
 
 size_t strlen(const char *);
 char *strncpy(char *dest, const char *src, size_t n);
+int strcmp(const char *s1, const char *s2);
 int strncmp(const char *s1, const char *s2, size_t n);
 
 void *memcpy(void *dest, const void *src, size_t n);
--- lib/string.c
+++ lib/string.c
@@ -27,6 +27,23 @@ char *strncpy(char *dest, const char *src, size_t n)
     return dest;  
 }
 
+int strcmp(const char *s1, const char *s2)
+{
+    for (; *s1; s1++, s2++) {
+        if (!*s2)
+            return 1;
+
+        if (*s2 > *s1)
+            return -1;
+
+        if (*s1 > *s2)
+            return 1;
+
+    }
+
+    return 0;
+}
+
 int strncmp(const char *s1, const char *s2, size_t n)
 {
     size_t i;
